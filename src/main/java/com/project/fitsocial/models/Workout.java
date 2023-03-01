b13117ad4220aaa92c2adbcd96a640e2cbe8a71e
vcntu <vcngit@gmail.com>
2023-03-01 08:34:54 -0800

    added workout model

---
+++ src/main/java/com/project/fitsocial/models/Workout.java
@@ -0,0 +1,147 @@
+package com.project.fitsocial.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="workouts")
+public class Workout {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String type;
+
+    @NotBlank
+    private String targetMuscle;
+
+    @NotBlank
+    private String equipment;
+
+    @NotBlank
+    private String difficulty;
+
+    @NotBlank
+    private String instructions;
+
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+
+
+    public Workout() {
+    }
+
+    public Workout(Long id, String title, String type, String targetMuscle, String equipment, String difficulty, String instructions, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.type = type;
+        this.targetMuscle = targetMuscle;
+        this.equipment = equipment;
+        this.difficulty = difficulty;
+        this.instructions = instructions;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTargetMuscle() {
+        return this.targetMuscle;
+    }
+
+    public void setTargetMuscle(String targetMuscle) {
+        this.targetMuscle = targetMuscle;
+    }
+
+    public String getEquipment() {
+        return this.equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getDifficulty() {
+        return this.difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getInstructions() {
+        return this.instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+}
